503fb9f21d14543e74f2a0de565592dd4fbf22b0
guikts <guilhermedurande@gmail.com>
2026-03-30 11:02:10 -0300

    modificação em routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TccController;
+use App\Http\Controllers\BancaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('clientes', TccController::class);
+Route::resource('vendas', BancaController::class);
